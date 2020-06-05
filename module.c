294c51510f89e414d6cd701b799479b5d6148439
Giampiero <giampiero7@users.noreply.github.com>
2020-06-05 14:48:11 +0200

    removed analog_temp/ptp

--- module.c
+++ module.c
@@ -1057,29 +1057,6 @@ static struct DeviceAttrBean devAttrBeansAnalogTemp[] = {
 
 	{
 		.devAttr = {
-			.attr = {
-				.name = "ptp",
-				.mode = 0660,
-			},
-			.show = devAttrI2c_show,
-			.store = devAttrI2c_store,
-		},
-		.regSpecsShow = {
-			.reg = 32,
-			.len = 2,
-			.maskedReg = false,
-			.mask = 0b1,
-			.shift = 2,
-			.sign = false,
-			.vals = NULL,
-		},
-		.regSpecsStore = {
-			.reg = -1,
-		},
-	},
-
-	{
-		.devAttr = {
 			.attr = { .name = NULL }
 		}
 	}
