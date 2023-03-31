06ea7d59eddb1e0604ffc611bdee90fd640ae966
Giampiero Baggiani <giampiero7@users.noreply.github.com>
2023-03-31 11:07:19 +0200

    Fix for kstrtobool() modification in kernel 6.1

--- module.c
+++ module.c
@@ -57,7 +57,7 @@
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Sfera Labs - http://sferalabs.cc");
 MODULE_DESCRIPTION("Iono Pi Max driver module");
-MODULE_VERSION("1.12");
+MODULE_VERSION("1.13");
 
 struct DeviceAttrRegSpecs {
 	uint16_t reg;
